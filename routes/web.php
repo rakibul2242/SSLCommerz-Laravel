a9b537f4435381ac9ef6269a82e6a8e27833e2d4
rakibul2242 <mdrakibulislam2242@gmail.com>
2025-08-19 00:13:26 +0600

    issu solve

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,16 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SslCommerzPaymentController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/example1', [SslCommerzPaymentController::class, 'exampleEasyCheckout']);
 
 // SSLCOMMERZ Start
 Route::get('/example1', [SslCommerzPaymentController::class, 'exampleEasyCheckout']);
 Route::get('/example2', [SslCommerzPaymentController::class, 'exampleHostedCheckout']);
-Route::get('/pay', [SslCommerzPaymentController::class, 'index']);
+Route::post('/pay', [SslCommerzPaymentController::class, 'index']);
 Route::post('/pay-via-ajax', [SslCommerzPaymentController::class, 'payViaAjax']);
-Route::get('/success', [SslCommerzPaymentController::class, 'success']);
-Route::get('/fail', [SslCommerzPaymentController::class, 'fail']);
-Route::get('/cancel', [SslCommerzPaymentController::class, 'cancel']);
-Route::get('/ipn', [SslCommerzPaymentController::class, 'ipn']);
+Route::post('/success', [SslCommerzPaymentController::class, 'success']);
+Route::post('/fail', [SslCommerzPaymentController::class, 'fail']);
+Route::post('/cancel', [SslCommerzPaymentController::class, 'cancel']);
+Route::post('/ipn', [SslCommerzPaymentController::class, 'ipn']);
 //SSLCOMMERZ END
